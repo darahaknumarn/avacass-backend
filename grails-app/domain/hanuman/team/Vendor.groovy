5de8759df5d51765c0748977fe31b341ec8de04b
Pisey Phal <36697905+PiseyPhal@users.noreply.github.com>
2021-07-24 12:36:58 +0700

    Added Vendor
    - added field vendor

--- grails-app/domain/hanuman/team/Vendor.groovy
+++ grails-app/domain/hanuman/team/Vendor.groovy
@@ -5,6 +5,8 @@ import hanuman.team.base.BaseDomain
 class Vendor extends BaseDomain {
     String name
     String description
+    Boolean isPopular = false
+    String imageUrl
 
     static constraints = {
         name nullable:true
@@ -13,5 +15,6 @@ class Vendor extends BaseDomain {
         createdBy nullable:true
         lastUpdateBy nullable:true
         description sqlType:'text' , nullable:true
+        imageUrl nullable: true
     }
 }
